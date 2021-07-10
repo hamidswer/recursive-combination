e43db221666b6c5755414bc5aeb7509d9d29c9e0
Hamid Zamaninasab <86551137+hamidswer@users.noreply.github.com>
2021-07-10 01:19:56 -0700

    Create recurisive_combination.py
    It's just a simple recursive combination in python

---
+++ recurisive_combination.py
@@ -0,0 +1,11 @@
+def recurisive_combination(n,m):
+    combination_result = 0
+    if (m==0 or m==n):
+        combination_result = 1
+    else:
+        combination_result = recurisive_combination(n-1,m) + recurisive_combination(n-1,m-1)
+    return combination_result
+
+
+print(recurisive_combination(4,2))
+# 6
